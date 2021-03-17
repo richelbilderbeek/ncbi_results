9632ed004483375b6116f93959e65caf54a67fc3
richelbilderbeek <richel@richelbilderbeek.nl>
2021-03-17 13:36:49 +0100

    Count SNPs strictly

--- R/plot_snps_per_gene_name_processed.R
+++ R/plot_snps_per_gene_name_processed.R
@@ -3,50 +3,69 @@
 #' @export
 plot_snps_per_gene_name_processed <- function(folder_name = folder_name) {
 
-  variations_csv_filenames <- list.files(
-    path = folder_name,
-    pattern = ".*_variations\\.csv$",
-    full.names = TRUE
-  )
+  results_filename <- file.path(folder_name, "results.csv")
+  testthat::expect_true(file.exists(results_filename))
+  t_results <- ncbiperegrine::read_results_file(results_filename)
 
-  testthat::expect_equal(1129, length(variations_csv_filenames))
-  gene_names <- stringr::str_replace(
-    string = basename(variations_csv_filenames),
-    pattern = "_variations.csv",
-    replacement = ""
-  )
-  testthat::expect_equal(1129, length(gene_names))
-  n_gene_names <- length(gene_names)
-  testthat::expect_equal(1129, n_gene_names)
+  if (1 == 2) {
+    variations_csv_filenames <- list.files(
+      path = folder_name,
+      pattern = ".*_variations\\.csv$",
+      full.names = TRUE
+    )
 
-  t_variations <- ncbiperegrine::read_variations_csv_files(variations_csv_filenames)
-  testthat::expect_equal(60683, nrow(t_variations))
-  unique_gene_ids <- unique(t_variations$gene_id)
-  testthat::expect_equal(951, length(unique_gene_ids))
-  n_gene_names_with_protein_variations <- length(unique_gene_ids)
-  testthat::expect_equal(951, n_gene_names_with_protein_variations)
+    testthat::expect_equal(1131, length(variations_csv_filenames))
+    gene_names <- stringr::str_replace(
+      string = basename(variations_csv_filenames),
+      pattern = "_variations.csv",
+      replacement = ""
+    )
+    testthat::expect_equal(1131, length(gene_names))
+    n_gene_names <- length(gene_names)
+    testthat::expect_equal(1131, n_gene_names)
 
-  # The missing genes have no SNPs that change the protein
-  missing_genes <- gene_names[!gene_names %in% unique_gene_ids]
-  testthat::expect_equal(178, length(missing_genes))
+    t_variations <- ncbiperegrine::read_variations_csv_files(variations_csv_filenames)
+    testthat::expect_equal(60683, nrow(t_variations))
+    unique_gene_ids <- unique(t_variations$gene_id)
+    testthat::expect_equal(951, length(unique_gene_ids))
+    n_gene_names_with_protein_variations <- length(unique_gene_ids)
+    testthat::expect_equal(951, n_gene_names_with_protein_variations)
 
-  tally_protein_variations <- dplyr::group_by(t_variations, gene_id) %>%
+    # The missing genes have no SNPs that change the protein
+    missing_genes <- gene_names[!gene_names %in% unique_gene_ids]
+    testthat::expect_equal(178, length(missing_genes))
+  }
+
+  tally_protein_variations <- dplyr::group_by(t_results, gene_id) %>%
     dplyr::summarize(n = dplyr::n())
   n_protein_variations <- sum(tally_protein_variations$n)
-  testthat::expect_equal(60683, n_protein_variations)
+  testthat::expect_equal(
+    n_protein_variations,
+    ncbiresults::get_n_variations_raw()
+  )
   tally_protein_variations$type <- "protein variation"
 
-  t_snps <- dplyr::filter(t_variations, ncbi::are_snps(variation))
-  readr::write_csv(dplyr::select(t_snps, variation), "~/t_snps.csv")
-  testthat::expect_equal(nrow(t_snps), 38967)
-  tally_snps <- t_snps %>%
-    dplyr::group_by(gene_id) %>%
-    dplyr::summarize(n = dplyr::n())
+  t_snps <- dplyr::filter(t_results, ncbi::are_snps(variation))
+  testthat::expect_equal(
+    nrow(t_snps),
+    ncbiresults::get_n_variations()
+  )
+  tally_snps <- dplyr::summarize(
+    dplyr::group_by(t_snps, gene_id),
+    n = dplyr::n()
+  )
   n_snps <- sum(tally_snps$n)
-  testthat::expect_equal(38967, n_snps)
+  testthat::expect_equal(
+    ncbiresults::get_n_variations(),
+    n_snps
+  )
   n_proteins_with_snps <- nrow(tally_snps)
-  testthat::expect_equal(912, n_proteins_with_snps)
+  testthat::expect_equal(
+    ncbiresults::get_n_unique_gene_ids(),
+    n_proteins_with_snps
+  )
   tally_snps$type <- "SNP"
+
 
   tally <- dplyr::bind_rows(tally_protein_variations, tally_snps)
   tally$type <- as.factor(tally$type)
@@ -61,11 +80,8 @@ plot_snps_per_gene_name_processed <- function(folder_name = folder_name) {
   ggplot2::labs(
     title = "Number of variations per gene name processed",
     caption = paste0(
-      "Number of gene names: ", n_gene_names, "\n",
-      "Number of gene names with protein variations: ", n_gene_names_with_protein_variations, "\n",
-      "Number of gene names with SNPs: ", n_proteins_with_snps, "\n",
-      "Total number of protein variations: ", n_protein_variations, "\n",
-      "Total number of SNPs: ", n_snps, "\n",
+      "Total number of protein variations: ", sum(tally_protein_variations$n), "\n",
+      "Total number of SNPs: ", sum(tally_snps$n), "\n",
       "Accession date: 2020-12-14"
     )
   )
